4541df14831c8dd03c323b3160571afd24fcc876
Nick <nick@NickBook.(none)>
2013-03-15 22:25:09 -0400

    don't inject resources

--- src/main/java/com/github/gwtbootstrap/client/ui/resources/ResourceInjector.java
+++ src/main/java/com/github/gwtbootstrap/client/ui/resources/ResourceInjector.java
@@ -34,78 +34,77 @@ import com.google.gwt.resources.client.TextResource;
  */
 public class ResourceInjector {
 
-    private static final Configurator ADAPTER = GWT.create(Configurator.class);
+  private static final Configurator ADAPTER = GWT.create(Configurator.class);
 
-    private static HeadElement head;
+  private static HeadElement head;
 
-    /**
-     * Injects the required CSS styles and JavaScript files into the document header.
-     * <pre>
+  /**
+   * Injects the required CSS styles and JavaScript files into the document header.
+   * <pre>
      * It's for NoStyle Module.
      * </pre>
-     */
-    public static void configureWithCssFile() {
-        
-     //   injectResourceCssAsFile("bootstrap.min.css");
-     //   injectResourceCssAsFile("gwt-bootstrap.css");
-     //   injectResourceCssAsFile("font-awesome.css");
+   */
+  public static void configureWithCssFile() {
 
-        configure();
-        
-    }
-    
-    /**
+    // injectResourceCssAsFile("bootstrap.min.css");
+    // injectResourceCssAsFile("gwt-bootstrap.css");
+    // injectResourceCssAsFile("font-awesome.css");
+
+    configure();
+
+  }
+
+  /**
      * Injects the required CSS styles and JavaScript files into the document
      * header.
-     */
-    public static void configure() {
+   */
+  public static void configure() {
 
-        Resources res = ADAPTER.getResources();
-        if (ADAPTER.hasResponsiveDesign())
-            activateResponsiveDesign(res);
-      //  injectJs(res.jquery());
-        injectJs(res.bootstrapJs());
-        injectCss(res.bootstrapCss());
-        injectCss(res.fontAwesomeCss());
-        injectCss(res.gwtBootstrapCss());
+    // Resources res = ADAPTER.getResources();
+    // if (ADAPTER.hasResponsiveDesign())
+    // activateResponsiveDesign(res);
+    // injectJs(res.jquery());
+    // injectJs(res.bootstrapJs());
+    // injectCss(res.bootstrapCss());
+    // injectCss(res.fontAwesomeCss());
+    // injectCss(res.gwtBootstrapCss());
 
+  }
 
+  private static void injectCss(TextResource r) {
+    StyleInjector.inject(r.getText());
+  }
+
+  /**
+   * Inject public resource css file as a file.
+   * @param filename inject file name
+   */
+  public static void injectResourceCssAsFile(String filename) {
+    LinkElement link = Document.get().createLinkElement();
+    link.setType("text/css");
+    link.setRel("stylesheet");
+    // link.setHref(GWT.getModuleName() + "/css/" + filename);
+    getHead().appendChild(link);
+  }
+
+  private static HeadElement getHead() {
+    if (head == null) {
+      Element elt = Document.get().getElementsByTagName("head").getItem(0);
+      assert elt != null : "The host HTML page does not have a <head> element"
+          + " which is required by StyleInjector";
+      head = HeadElement.as(elt);
     }
+    return head;
+  }
 
-    private static void injectCss(TextResource r) {
-        StyleInjector.inject(r.getText());
-    }
+  private static void injectJs(TextResource r) {
+    JavaScriptInjector.inject(r.getText());
+  }
 
-    /**
-     * Inject public resource css file as a file.
-     * @param filename inject file name
-     */
-    public static void injectResourceCssAsFile(String filename) {
-        LinkElement link = Document.get().createLinkElement();
-        link.setType("text/css");
-        link.setRel("stylesheet");
-       // link.setHref(GWT.getModuleName() + "/css/" + filename);
-        getHead().appendChild(link);
-    }
-
-    private static HeadElement getHead() {
-        if (head == null) {
-            Element elt = Document.get().getElementsByTagName("head").getItem(0);
-            assert elt != null : "The host HTML page does not have a <head> element"
-                    + " which is required by StyleInjector";
-            head = HeadElement.as(elt);
-        }
-        return head;
-    }
-
-    private static void injectJs(TextResource r) {
-        JavaScriptInjector.inject(r.getText());
-    }
-
-    private static void activateResponsiveDesign(Resources res) {
-        injectCss(res.bootstrapResponsiveCss());
+  private static void activateResponsiveDesign(Resources res) {
+    injectCss(res.bootstrapResponsiveCss());
         MetaInjector
                 .inject("viewport", "width=device-width, initial-scale=1.0");
-    }
+  }
 
 }
